c59201bf98721e8883c7a8f031df8bb3f6168564
Jaden Marcus Ren <ruirenjerry@gmail.com>
2019-12-24 23:27:10 -0800

    pca

---
+++ Machine learning fundamentals/PCA.py
@@ -0,0 +1,22 @@
+
+# reference:  python for machine learning
+
+import pandas as pd
+df_wine = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+    header = None)
+
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.3,
+                                                    stratify=y,
+                                                    random_state=0)
+
+# standardize the features 
+                                                            
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
